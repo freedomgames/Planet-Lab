49462588d00bc8857a3facbc90c744b3c29df730
waltaskew <waltaskew@gmail.com>
2013-11-07 19:27:42 -0600

    take database from os.environ['DATABASE_URL']

--- backend/src/backend/config.py
+++ backend/src/backend/config.py
@@ -8,5 +8,4 @@ DEBUG = bool(os.environ.get('DEBUG', True))
 SECRET_KEY = os.environ.get(
         'SECRET_KEY',
         '\xc9\xabf\x18\xe4nL`t\xf5G\x86\xd6?\xde\xd9>6\x00Z\xdc\xd3\x82h')
-SQLALCHEMY_DATABASE_URI = os.environ.get(
-        'SQLALCHEMY_DATABASE_URI', 'sqlite:///test.db')
+SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
