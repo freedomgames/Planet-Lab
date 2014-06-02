50dcee16a8ccf04be222fb08971d9da6ebca4352
waltaskew <waltaskew@gmail.com>
2014-06-02 13:55:04 -0500

    sane default config values

--- backend/src/backend/config.py
+++ backend/src/backend/config.py
@@ -4,8 +4,6 @@
 import os
 
 
-DEBUG = bool(os.environ.get('DEBUG', True))
-SECRET_KEY = os.environ.get(
-        'SECRET_KEY',
-        '\xc9\xabf\x18\xe4nL`t\xf5G\x86\xd6?\xde\xd9>6\x00Z\xdc\xd3\x82h')
+DEBUG = bool(os.environ.get('DEBUG', False))
+SECRET_KEY = os.environ.get('SECRET_KEY', 'snakes')
 SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
