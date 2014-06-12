932c593e2b84c2f2a70551a4e4c1020cbb86e326
waltaskew <waltaskew@gmail.com>
2014-06-12 15:34:33 -0500

    factor our simple many-to-many code

--- backend/src/backend/__init__.py
+++ backend/src/backend/__init__.py
@@ -73,7 +73,7 @@ api.add_resource(quest_views.QuestUserList, '/v1/users/<int:user_id>/quests/')
 
 api.add_resource(
         quest_views.QuestMissionLink,
-        '/v1/missions/<int:mission_id>/quests/<int:quest_id>')
+        '/v1/missions/<int:left_id>/quests/<int:right_id>')
 api.add_resource(
         quest_views.QuestMissionLinkList,
         '/v1/missions/<int:mission_id>/quests/')
