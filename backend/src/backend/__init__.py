5b38c743d3b29ecd8ae2754930cd79115815db84
waltaskew <waltaskew@gmail.com>
2014-06-13 14:10:27 -0500

    return simple templates for index, login & the app

--- backend/src/backend/__init__.py
+++ backend/src/backend/__init__.py
@@ -7,8 +7,6 @@ import flask_sqlalchemy
 import logging
 import traceback
 
-import backend.config as config
-
 
 app = flask.Flask(__name__)
 app.config.from_object('backend.config')
@@ -23,10 +21,48 @@ if not app.debug:
     app.logger.setLevel(logging.INFO)
 
 
+import backend.common.auth as auth
+import backend.missions.views as mission_views
+import backend.oauth.views
+import backend.organizations.views as organization_views
+import backend.quests.views as quest_views
+import backend.users.models as user_models
+import backend.users.views as user_views
+
+
 @app.route('/')
 def index():
     """Return the index page."""
-    return "Hello"
+    return flask.render_template('index.html')
+
+
+@app.route('/login')
+def login():
+    """Return the login page."""
+    return flask.render_template('login.html')
+
+
+@app.route('/app')
+@auth.login_required
+def app_page():
+    """Return the javascript for the app and pass along information
+    on the currently logged-in user.
+    """
+    user_id = auth.current_user_id()
+    user_row = db.session.query(
+            user_models.User.name, user_models.User.email).filter_by(
+                    id=user_id).first()
+    if user_row is None:
+        # this really should not happen -- the user would have had
+        # to have been deleted without killing the session
+        return flask.redirect(flask.url_for('login'))
+    else:
+        return flask.render_template(
+                'app.html',
+                user_id=user_id,
+                user_name=user_row[0],
+                user_email=user_row[1],
+                user_url=api.url_for(user_views.User, user_id=user_id))
 
 
 def error_handler(error, status_code=500, payload=None, debug=app.debug):
@@ -55,11 +91,6 @@ def other_error(error):
     """
     return error_handler(error, payload={'type': 'general error'})
 
-
-import backend.users.views as user_views
-import backend.missions.views as mission_views
-import backend.organizations.views as organization_views
-import backend.quests.views as quest_views
 
 api.add_resource(user_views.User, '/v1/users/<int:user_id>')
 api.add_resource(user_views.UserList, '/v1/users/')
