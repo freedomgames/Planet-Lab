72908179aeb6e426850c9ff34a1192ad2f64ff7a
waltaskew <waltaskew@gmail.com>
2014-06-17 16:14:33 -0500

    swap in flask-user stuff for oauth

--- backend/src/backend/config.py
+++ backend/src/backend/config.py
@@ -7,3 +7,4 @@ import os
 DEBUG = bool(os.environ.get('DEBUG', False))
 SECRET_KEY = os.environ.get('SECRET_KEY', 'snakes')
 SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+USER_ENABLE_EMAIL = bool(os.environ.get('USER_ENABLE_EMAIL', True))
