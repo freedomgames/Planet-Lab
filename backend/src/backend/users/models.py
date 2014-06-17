660707ecf7cbe4fe5f38b2fafaa1de347c847a66
waltaskew <waltaskew@gmail.com>
2014-06-17 16:14:33 -0500

    swap in flask-user stuff for oauth

--- backend/src/backend/users/models.py
+++ backend/src/backend/users/models.py
@@ -1,37 +1,33 @@
 """SQLAlchemy models for users"""
 
 
+import flask_user
+
 import backend
 
 db = backend.db
 GOOGLE_OAUTH_TYPE = "google"
 
 
-class User(db.Model):
+class User(db.Model, flask_user.UserMixin):
     """A user account for either a learner or a mentor."""
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True, nullable=False)
-    name = db.Column(db.String, nullable=False)
+    active = db.Column(db.Boolean, nullable=False, default=False, index=True)
+    username = db.Column(db.String, nullable=False, unique=True, index=True)
+    password = db.Column(db.String, nullable=False, default='', index=True)
+
+    name = db.Column(db.String, nullable=True)
     email = db.Column(db.String, nullable=True)
     description = db.Column(db.String, nullable=True)
     avatar_url = db.Column(db.String, nullable=True)
-
-    oauth_id = db.Column(db.String, nullable=True)
-    oauth_type = db.Column(
-            db.Enum(GOOGLE_OAUTH_TYPE, name='oauth_types'),
-            nullable=True)
-    oauth_token = db.Column(db.String, nullable=True)
 
     missions = db.relationship("Mission", backref="user")
     quests = db.relationship("Quest", backref="user")
     organizations_created = db.relationship("Organization", backref="user")
 
-    __table_args__ = (db.UniqueConstraint('oauth_id', 'oauth_type'),)
-
     @property
     def url(self):
         """URL for the resource."""
         return backend.api.url_for(backend.user_views.User, user_id=self.id)
-
-db.Index('ix_user_oauth', User.oauth_id, User.oauth_type)
