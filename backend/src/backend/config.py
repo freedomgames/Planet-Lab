f18401ef7373d08d64042683f1cc4a162e41306d
waltaskew <waltaskew@gmail.com>
2014-06-24 15:55:53 -0500

    update configurable values
    - make SECRET_KEY a required config value
    - add s3 access config

--- backend/src/backend/config.py
+++ backend/src/backend/config.py
@@ -4,7 +4,10 @@
 import os
 
 
+AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
+AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
 DEBUG = bool(os.environ.get('DEBUG', False))
-SECRET_KEY = os.environ.get('SECRET_KEY', 'snakes')
+S3_BUCKET = os.environ['S3_BUCKET']
+SECRET_KEY = os.environ['SECRET_KEY']
 SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 USER_ENABLE_EMAIL = bool(os.environ.get('USER_ENABLE_EMAIL', True))
