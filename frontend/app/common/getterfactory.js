4684ee80bd6fe0c90d0a107da7286ee932fa4358
waltaskew <waltaskew@gmail.com>
2014-07-15 12:57:56 -0500

    whitespace

--- frontend/app/common/getterfactory.js
+++ frontend/app/common/getterfactory.js
@@ -1,7 +1,7 @@
 planet_app.factory('GetterFactory', ['$resource', function ($resource) {
     return {
         getItem: function(resource, id) {
-            return $resource( '/v1/' + resource + '/:id',  {},  {
+            return $resource('/v1/' + resource + '/:id',  {},  {
                 query: {
                     method:'GET', 
                     params: {
