2bca5c0cd3826c85a9f38a989007f6ca8b367eb3
waltaskew <waltaskew@gmail.com>
2014-07-15 15:25:28 -0500

    add in-line editting

--- frontend/app/app.js
+++ frontend/app/app.js
@@ -1,4 +1,5 @@
-var planet_app = angular.module('planet_app', ['ui.router', 'ngResource']);
+var planet_app = angular.module(
+        'planet_app', ['ui.router', 'ngResource', 'xeditable']);
 
 planet_app.config(['$controllerProvider', '$provide', '$compileProvider', '$stateProvider', '$urlRouterProvider', function($controllerProvider, $provide, $compileProvider, $stateProvider, $urlRouterProvider){
     /* CODE FOR ASYNC MODULE LOADING */
--- frontend/app/quest/controller.js
+++ frontend/app/quest/controller.js
@@ -1,9 +1,4 @@
 planet_app.controller('QuestCtrl', [
     '$scope', 'ResourceFactory', function ($scope, ResourceFactory) {
         $scope.quest = ResourceFactory('quests').get();
-        $scope.update = function() {
-            if ($scope.questForm.$valid) {
-                $scope.quest.$put();
-            }
-        };
 }]);
