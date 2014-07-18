38bc4f0e993568c6261da8bc86e436624f7c4dd8
waltaskew <waltaskew@gmail.com>
2014-07-18 12:49:10 -0500

    move s3 upload code into a service

--- frontend/app/quest/controller.js
+++ frontend/app/quest/controller.js
@@ -1,63 +1,26 @@
-var updateScope = function($scope, $upload, S3Factory) {
-    // Set common functions required by views of both controllers
-
-    var s3Upload = function(file, uploadData) {
-        // Given the file to upload an an object containing the form
-        // data needed to upload the file to S3, perform the upload.
-        // Then set the quest's icon URL to the just-uploaded file.
-        uploadData.upload_args.file = file;
-        $upload.upload(uploadData.upload_args).then(
-            function(response) {
-                if (response.status === 201) {
-                    $scope.quest.icon_url = uploadData.s3_url;
-                    alert('Upload Suceeded');
-                } else {
-                    alert('Upload Failed');
-                }
+var questCtrlUtil = {
+    upload: function($files, quest, S3) {
+        S3.upload($files[0], 'quests', quest.id, 'uploads').then(
+            function(icon_url) {quest.icon_url = icon_url;
         });
-    };
-    var beginUpload = function($files) {
-        // Request the form data required to upload the file to S3
-        // from the backend and then perform the upload.
-        for (var i=0; i < $files.length; i++) {
-            var file = $files[i];
-            url = S3Factory('quests').get({
-                id: $scope.quest.id,
-                fileName: file.name,
-                uploadName: 'uploads',
-                mime_type: file.type});
-            url.$promise.then(function() {
-                s3Upload(file, url);
-            });
-        }
-    };
-    $scope.onFileSelect = function($files) {
-        // Handle file uploads by sending them to S3.
-        if (! $scope.quest.id) {
-            // In case we are working with a new, un-saved quest.
-            // We need an id to upload quest assets to S3
-            $scope.quest.$save().then(function() {
-                beginUpload($files);
-            });
-        } else {
-            beginUpload($files);
-        }
     }
 };
 
 planet_app.controller('QuestCtrl', [
-    '$scope', '$stateParams', '$upload', 'ResourceFactory', 'S3Factory',
-    function($scope, $stateParams, $upload, ResourceFactory, S3Factory) {
+    '$scope', '$stateParams', 'ResourceFactory', 'S3',
+    function($scope, $stateParams, ResourceFactory, S3) {
         $scope.quest = ResourceFactory('quests').get({id: $stateParams.id})
         // have to wrap $scope.quest.$put in a new function as the promise
         // won't be back in time to do a $scope.save = $scope.quest.$put
         $scope.save = function() {$scope.quest.$put()};
-        updateScope($scope, $upload, S3Factory);
+        $scope.onFileSelect = function($files) {
+            questCtrlUtil.upload($files, $scope.quest, S3);
+        };
 }]);
 
 planet_app.controller('NewQuestCtrl', [
-    '$scope', '$upload', 'ResourceFactory', 'S3Factory',
-    function($scope, $upload, ResourceFactory, S3Factory) {
+    '$scope', 'ResourceFactory', 'S3',
+    function($scope, ResourceFactory, S3) {
         $scope.quest = new (ResourceFactory('quests'));
         $scope.save = function() {
             if ($scope.quest.id) {
@@ -66,5 +29,14 @@ planet_app.controller('NewQuestCtrl', [
                 $scope.quest.$save();
             }
         };
-        updateScope($scope, $upload, S3Factory);
+        $scope.onFileSelect = function($files) {
+            if (! $scope.quest.id) {
+                // We need an id to upload quest assets to S3
+                $scope.quest.$save().then(function() {
+                    questCtrlUtil.upload($files, $scope.quest, S3);
+                });
+            } else {
+                questCtrlUtil.upload($files, $scope.quest, S3);
+            }
+        };
 }]);
